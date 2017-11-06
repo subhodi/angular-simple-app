0654613d82ef1cb5dbad56ff942cb033e7f31ee7
Subhod I <subhodpoojary@gmail.com>
2017-11-06 18:43:05 +0530

    Typescript class

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -7,9 +7,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+    let a: number;
+    a = 10;
+    let array: any;
+    let str: string;
+    str = 'string';
+    array = ['string', 1, { 'key': 'value' }];
+
+    const colorRed = 0;
+    const colorGreen = 1;
+
+    enum Color { Red = 0, Green = 1 }
+    let backgroundColor = Color.Green;
+  }
 
   ngOnInit() {
   }
 
 }
+
+class About {
+  constructor(private _title: string, private _year: number) {
+  }
+
+  printAbout() {
+    return this._title + ' | ' + this._year;
+  }
+
+  get title() {
+    return this._title;
+  }
+
+  set title(value) {
+    this._title = value;
+  }
+
+  get year() {
+    return this._year;
+  }
+
+  set year(value) {
+    this._year = value;
+  }
+}
+
+const about = new About('PSL', 2017);
+console.log(about.printAbout());
+about.title = 'Persistent systems limited.';
+console.log(about.title);
+
+
